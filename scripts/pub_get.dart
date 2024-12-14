73b093e2df7783b51a8f360cad94433c8359de14
Ellet <echo.ellet@gmail.com>
2024-12-14 23:09:53 +0300

    ci: set runInShell to true for pub_get.dart

--- scripts/pub_get.dart
+++ scripts/pub_get.dart
@@ -4,6 +4,10 @@ import 'packages.dart';
 
 void main(List<String> args) {
   for (final package in packages) {
-    Process.runSync('flutter', ['pub', 'get'], workingDirectory: package);
+    Process.runSync(
+      'flutter',
+      ['pub', 'get', '-C', package],
+      runInShell: true,
+    );
   }
 }
