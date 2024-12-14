ca226649a91282591317487897bbf3ebcaf9ecff
Ellet <echo.ellet@gmail.com>
2024-12-14 15:56:13 +0300

    chore: format Swift files for testing CI

--- quill_native_bridge_ios/ios/quill_native_bridge_ios/Sources/quill_native_bridge_ios/QuillNativeBridgePlugin.swift
+++ quill_native_bridge_ios/ios/quill_native_bridge_ios/Sources/quill_native_bridge_ios/QuillNativeBridgePlugin.swift
@@ -5,6 +5,6 @@ public class QuillNativeBridgePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let messenger = registrar.messenger()
     let api = QuillNativeBridgeImpl()
-          QuillNativeBridgeApiSetup.setUp(binaryMessenger: messenger, api: api)
+    QuillNativeBridgeApiSetup.setUp(binaryMessenger: messenger, api: api)
   }
 }
