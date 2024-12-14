08c4a36802c4171a37cf427375337bf71cb9d23e
Ellet <echo.ellet@gmail.com>
2024-12-14 16:29:47 +0300

    ci: fix swift-format cache

--- quill_native_bridge_ios/ios/quill_native_bridge_ios/Sources/quill_native_bridge_ios/QuillNativeBridgePlugin.swift
+++ quill_native_bridge_ios/ios/quill_native_bridge_ios/Sources/quill_native_bridge_ios/QuillNativeBridgePlugin.swift
@@ -4,7 +4,7 @@ import UIKit
 public class QuillNativeBridgePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let messenger = registrar.messenger()
-      let api = QuillNativeBridgeImpl()
+    let api = QuillNativeBridgeImpl()
     QuillNativeBridgeApiSetup.setUp(binaryMessenger: messenger, api: api)
   }
 }
